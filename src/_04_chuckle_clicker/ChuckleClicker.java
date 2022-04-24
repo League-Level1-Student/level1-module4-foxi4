6c57190a260734d230029a23af4e5cbbd9beb0d0
laurenkan. <laurenkan.@192.168.4.29>
2022-04-24 14:03:04 -0700

    4.24.22

--- src/_04_chuckle_clicker/ChuckleClicker.java
+++ src/_04_chuckle_clicker/ChuckleClicker.java
@@ -1,7 +1,24 @@
 package _04_chuckle_clicker;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class ChuckleClicker {
-public static void main(String[] args) {
-	//public void makeButton();
+static void makeButtons() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton joke = new JButton();
+		JButton punchline = new JButton();
+		joke.setText("joke");
+		punchline.setText("punchline");
+		frame.setTitle("Choose one");
+		frame.add(panel);
+		frame.add(joke);
+		frame.add(punchline);
+		frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+	makeButtons();
 }
 }
