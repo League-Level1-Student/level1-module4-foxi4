c6c0f85d29c7d1c89773aa07099b576a02f6bd50
laurenkan. <laurenkan.@localhost>
2022-05-01 13:52:25 -0700

    5.1.22

--- src/_04_chuckle_clicker/ChuckleClicker.java
+++ src/_04_chuckle_clicker/ChuckleClicker.java
@@ -2,6 +2,7 @@ package _04_chuckle_clicker;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class ChuckleClicker {
@@ -13,12 +14,20 @@ static void makeButtons() {
 		joke.setText("joke");
 		punchline.setText("punchline");
 		frame.setTitle("Choose one");
+		frame.setVisible(true);
 		frame.add(panel);
 		frame.add(joke);
 		frame.add(punchline);
-		frame.setVisible(true);
+		joke.addActionListener(null);
+		punchline.addActionListener(null);
+		//if(event.getSource() == joke);
+		//if(joke == getPressed()) {
+			JOptionPane.showMessageDialog(null, "hi");
+		//}
+		
 	}
 	public static void main(String[] args) {
 	makeButtons();
+	
 }
 }
